36b7a169cf08b7079a0f76c741d7cf64f0767b07
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-12-29 03:49:18 +0000

    Automatically backed up by Learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -4,7 +4,7 @@ module Players
     
     def move(board)
       
-<<<<<<< HEAD
+
       puts "Please enter your next move"
       input = gets.strip
       if (board.valid_move?(input))
@@ -13,10 +13,10 @@ module Players
         puts "Invalid"
         move(board)
       end
-=======
+
       input = gets.strip
       board.update(input,self)
->>>>>>> 58f166b964ea7e93777e795096a8383d07dbd252
+
       input
       
     end
